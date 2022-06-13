0e4e86ab5a025cac2c0cdbb604fd7ee7809f6ad2
wander <1194409532@qq.com>
2022-06-13 20:03:29 +0800

    metadata

--- src/main/java/com/github/wanderros/encrypt/starter/config/EncryptProperties.java
+++ src/main/java/com/github/wanderros/encrypt/starter/config/EncryptProperties.java
@@ -5,6 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "spring.encrypt")
 public class EncryptProperties {
     private final static String DEFAULT_KEY = "www.itboyhub.com";
+    /**
+     * 默认加解密秘钥
+     */
     private String key = DEFAULT_KEY;
 
     public String getKey() {
